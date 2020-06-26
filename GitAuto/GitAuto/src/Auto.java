dbedb25cb1884d6a0d9f7c0c7837aaa346c73e57
preethikishor <30550110+preethikishor@users.noreply.github.com>
2020-06-27 00:26:02 +0530

    Update Auto.java

--- GitAuto/GitAuto/src/Auto.java
+++ GitAuto/GitAuto/src/Auto.java
@@ -2,7 +2,7 @@
 public class Auto {
 
 	public static void main(String[] args) {
-		System.out.println("Exception Auto 11111");
+		System.out.println("Exception Auto ddd 11111");
 		System.out.println("Exception Autoaaa sdsdsd");
 	}
 
